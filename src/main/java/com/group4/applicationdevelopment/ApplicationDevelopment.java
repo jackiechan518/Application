f7fe02db30710f55d8cb0fbb9d74fc817277beb3
MrThinkJ <ledinhthinh.ws@gmail.com>
2023-03-07 10:24:23 +0700

    aaa

---
+++ src/main/java/com/group4/applicationdevelopment/ApplicationDevelopment.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.group4.applicationdevelopment;
+
+/**
+ *
+ * @author ACER
+ */
+public class ApplicationDevelopment {
+
+    public static void main(String[] args) {
+//        PerimeterCalculator perimeterCalculator = new PerimeterCalculator();
+//        perimeterCalculator.run();
+        CalculatePerimeter calculatePerimeter = new CalculatePerimeter();
+        calculatePerimeter.setVisible(true);
+        
+    }
+}
